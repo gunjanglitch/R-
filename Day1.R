868061a5f879fa4c999c7577282bbda505677b25
gunjanglitch <gunjansarode.bioinfo@gmail.com>
2024-09-20 12:49:35 +0530

    Add files via upload

---
+++ Day1.R
@@ -0,0 +1,82 @@
+10+30
+x = 30
+y = 40
+z = x + y
+print(x)
+print(y)
+print(z)
+abc = 60
+pqr = 20
+xyz = pqr = abc
+abc
+pqr
+xyz
+ls()
+rm(pqr)
+pqr
+ls()
+
+#this is a comment
+a1 = c(10,20,30,40,50)
+a2 = c("gunjan", "milind", "embs")
+x = 45
+y = 24
+z = 34
+print(c(x,y,z))
+
+a5 = 1:50
+a5
+
+a6 = 10:1
+a6
+
+a8 = seq(from=2, to=20, by=2)
+a8
+
+a9 = seq(from=5, to=100, by=5)
+a9
+
+a10 = seq(from=1, to=20, length=30)
+a10
+
+x4=40
+x5=50
+x6=80
+x7=90
+x8=100
+x6=c(x4,x5,60,70,x7,x8)
+x6
+
+x7=rep("happy birthday", times=10)
+x7
+
+
+
+x8 = rep(c("bio","chemistry"), times=10)
+x8
+
+x9 = rep(c(4,7,9), times=5)
+x9
+
+x10 = rep(c(4,7,9), each=5)
+x10
+
+x11 = sample(10:30,7)
+x11
+
+x12=sample(10:20,20)
+x12=sample(10:20,20, replace = TRUE)
+x12
+
+
+g=c(10,20,30,40,50,60,70,80,90,100,110,120,130,140,150)
+length(g)
+g[1]
+g[4]
+y = g[6]
+y
+g[c(4,6,8,11)]
+g[3:10]
+g[3:length(g)]
+g[-5]
+g[-c(5,7,9,11)]
